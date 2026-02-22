ec7aeacdce045457b11e871cbb826cc5b1f317f2
YasmeenMuhammed <yasmeenmuhammed69@gmail.com>
2026-02-22 05:05:11 +0300

    last commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -110,7 +110,7 @@ export default function HeroSection() {
             <div className="flex flex-wrap items-center gap-6 sm:gap-8 lg:gap-12">
               <div className="flex-shrink-0">
                 <div className="flex items-baseline gap-1">
-                  <h3 className="text-3xl sm:text-4xl font-bold text-black mb-10">
+                  <h3 className="text-3xl sm:text-4xl font-bold text-black ">
                     {brandsCount}+
                   </h3>
                 </div>
@@ -153,7 +153,7 @@ export default function HeroSection() {
       </div>
 
       {/* Brand Marquee */}
-      <div className="absolute bottom-0 left-0 right-0 bg-black py-8 lg:py-10 overflow-hidden">
+      <div className="absolute bottom-0 left-0 right-0 bg-black py-8 lg:py-10 overflow-hidden mt-20">
         <div className="relative">
           {/* Infinite scroll animation */}
           <div className="flex animate-marquee">
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,27 @@
 import { decode } from "next-auth/jwt";
 import { cookies } from "next/headers";
 
-export async function getUserToken(){
-    const myCookies = await cookies()
-   const decodedToken = myCookies.get('next-auth.session-token')?.value || myCookies.get('__Secure-next-auth.session-token')?.value
-  const token = await decode({token:decodedToken , secret:process.env.AUTH_SECRET!})
-   console.log(decodedToken);
-   console.log(token);
-   console.log(token?.token);
-   
-    return token?.token as string
+export async function getUserToken() {
+  const myCookies = await cookies();
+
+  const decodedToken =
+    myCookies.get("next-auth.session-token")?.value ||
+    myCookies.get("__Secure-next-auth.session-token")?.value;
+
+  if (!decodedToken) {
+    console.log("No token found in cookies");
+    return null;
+  }
+
+
+  const token = await decode({
+    token: decodedToken,
+    secret: process.env.NEXTAUTH_SECRET!,
+  });
+
+  console.log("Raw token:", decodedToken);
+  console.log("Decoded token:", token);
+
+  // تقدر ترجّع الـ payload كله أو قيمة معينة زي الإيميل أو الـ id
+  return token;
 }
